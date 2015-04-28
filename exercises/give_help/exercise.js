128467cb5f34e162b95650cc8f9c7569106d418f
Steve Gangstead <steven@gangstead.com>
2015-04-27 21:00:17 -0500

    refactored streams

--- exercises/give_help/exercise.js
+++ exercises/give_help/exercise.js
@@ -24,31 +24,34 @@ exercise.addSetup(function (mode, callback) {
   process.nextTick(callback)
 })
 
-// Mess with std out stream.  Must be done *before* comparestdut
+// Mess with std out stream.  Must be done *before* comparestdout
 exercise.addProcessor(function (mode, callback) {
-  //use through or through2 to intercept stdout //TODO SWG
-  function cleanOutput() {return through2( function(buf, enc, callback){
-    var lines = buf.toString().split('\n');
 
-    function shouldTestLine(l) {
-      return (l.length > 0 && l.indexOf('Usage:') == -1)
-    }
+  // There are some lines we don't want to compare
+  function shouldTestLine(l) {
+    return (
+      l.length > 0                  //pointless to compare empty lines
+      && l.indexOf('Usage:') == -1  //This line contains the name of the submission file
+      )
+  }
 
-    this.push(lines.filter(shouldTestLine).join('\n'));
-    // if(line.length > 0
-    //   && line.indexOf('Usage:') != 0 ){
-    //     this.push(line)
-    // }
-    //this.push(line);
-    callback()
-  })};
+  //use through2 to intercept stdout and replace it with a stream that's been cleaned up for testing
+  function cleanOutput(dirtyStream, lineValidator) {
+    var cleanStream = through2( function(buf, enc, callback){
+      var lines = buf.toString().split('\n');
+      var cleanStr = lines.filter(lineValidator).join('\n');
+      this.push(cleanStr);
+      callback()
+    });
 
-  var dirtySubmissionOut = this.submissionStdout;
-  var dirtySolutionOut = this.solutionStdout;
-  this.submissionStdout = cleanOutput();
-  this.solutionStdout = cleanOutput();
-  dirtySubmissionOut.pipe(this.submissionStdout);
-  dirtySolutionOut.pipe(this.solutionStdout);
+    dirtyStream.pipe(cleanStream);
+    return cleanStream;
+  };
+
+  this.submissionStdout = cleanOutput(this.submissionStdout, shouldTestLine);
+  if (mode == 'verify') {
+    this.solutionStdout = cleanOutput(this.solutionStdout, shouldTestLine);
+  }
   process.nextTick(callback);
 })
 
